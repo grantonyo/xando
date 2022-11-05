268d968e52bd5d0969ce484be1063127601a44d0
GrAnt <granty1983@mail.ru>
2022-11-06 01:31:14 +0300

    Check function was added (peramter x and 0 yet needs to be implemented)

--- xando.py
+++ xando.py
@@ -26,3 +26,27 @@ def player2_turn_func(par):
     input_list = int(par[1]), dict[par[0]],
     matrix[input_list[0]][input_list[1]]="o"
     display_func(matrix)
+
+#4. Function that checks if the game ended:
+def completion_check_func(matrix):
+    col1, col2, col3 = [], [], [],
+    dgnl1, dgnl2 = [], []
+    cnt1, cnt2 = 0, 3
+    for row in matrix:
+        if row.count('x') == 3:
+            print("End of game. Player 1 is a winner")
+            break
+        else:
+            col1.append(row[1])
+            col2.append(row[2])
+            col3.append(row[3])
+            dgnl1.append(row[cnt1])
+            cnt1 += 1
+            dgnl2.append(row[cnt2])
+            cnt2 -= 1
+    if col1.count('x') == 3 or col2.count('x') == 3 or col3.count('x') == 3:
+        print("End of game. Player 1 is a winner")
+    elif dgnl1.count('x') == 3 or dgnl2.count('x') == 3:
+        print("End of game. Player 1 is a winner")
+
+
