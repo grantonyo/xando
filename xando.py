63cefa67ab989cecd799608ce2bdfc494be9c692
GrAnt <granty1983@mail.ru>
2022-11-06 20:50:10 +0300

    Turn change parameters and functions were added. Check function  was modified

--- xando.py
+++ xando.py
@@ -14,27 +14,29 @@ def display_func(matrix):
         print()
 display_func(matrix) #command that shows the initial xando filed
 
-#3. Functions that change xando field:
-def player1_turn_func(par):
-    dict = {'a': 1, 'b': 2, 'c': 3}
-    input_list = int(par[1]), dict[par[0]],
-    matrix[input_list[0]][input_list[1]]="x"
-    display_func(matrix)
+#3. Parameters  and function that changes player'sturn:
+player=1
+par='x'
+def xo_func():
+    if par=='x':
+        return 'x'
+    else:
+        return 'o'
 
-def player2_turn_func(par):
+def player_turn_func(inp):
     dict = {'a': 1, 'b': 2, 'c': 3}
-    input_list = int(par[1]), dict[par[0]],
-    matrix[input_list[0]][input_list[1]]="o"
+    input_list = int(inp[1]), dict[inp[0]],
+    matrix[input_list[0]][input_list[1]]=xo_func()
     display_func(matrix)
 
 #4. Function that checks if the game ended:
 def completion_check_func(matrix):
     col1, col2, col3 = [], [], [],
     dgnl1, dgnl2 = [], []
-    cnt1, cnt2 = 0, 3
+    cnt1, cnt2, cnt3 = 0, 3, 0
     for row in matrix:
-        if row.count('x') == 3:
-            print("End of game. Player 1 is a winner")
+        if row.count(par) == 3:
+            print(f'End of game. Player {player} is a winner')
             break
         else:
             col1.append(row[1])
@@ -44,9 +46,17 @@ def completion_check_func(matrix):
             cnt1 += 1
             dgnl2.append(row[cnt2])
             cnt2 -= 1
-    if col1.count('x') == 3 or col2.count('x') == 3 or col3.count('x') == 3:
-        print("End of game. Player 1 is a winner")
-    elif dgnl1.count('x') == 3 or dgnl2.count('x') == 3:
-        print("End of game. Player 1 is a winner")
+            if row.count("-")==0:
+                cnt3+=1
+    if col1.count(par) == 3 or col2.count(par) == 3 or col3.count(par) == 3:
+        print(f"End of game. Player {player} is a winner")
+    elif dgnl1.count(par) == 3 or dgnl2.count(par) == 3:
+        print(f"End of game. Player {player} is a winner")
+    elif cnt3==4:
+        print(f"End of game. Draw. No winner.")
+    else:
+        cnt3=0
+
+#5. Game engine
 
 
